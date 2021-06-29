68dea2b4c9c57cb282614b26a37f3efc7d9dee2a
Rodrigo Moreira <hola@piducan.dev>
2021-06-29 11:21:45 -0400

    add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `mimimi`,
+    description: `nueva poesía latinoamericana`,
+    author: `e-poetry.institute`,
   },
   plugins: [
     {
