a16c28ebf1f2e67ef9f8df27618bfcf0e125f0da
Rodrigo Moreira <hola@piducan.dev>
2021-07-02 15:31:50 -0400

    create form component

---
+++ src/components/form.js
@@ -0,0 +1,20 @@
+/** @jsx jsx */
+import { jsx, Box, Label, Input, Button } from "theme-ui"
+
+export default function Form() {
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(e)
+  }
+  return (
+    <Box sx={{ maxWidth: 640, mx: "auto" }} as="form" onSubmit={handleSubmit}>
+      <Label htmlFor="name">Seudónimo</Label>
+      <Input name="name" id="name" mb={3} />
+      <Label htmlFor="url">Enlace a la obra</Label>
+      <Input type="url" name="url" id="url" mb={3} />
+      <Label htmlFor="email">Correo electrónico</Label>
+      <Input type="email" name="email" id="email" mb={3} />
+      <Button>Enviar</Button>
+    </Box>
+  )
+}
